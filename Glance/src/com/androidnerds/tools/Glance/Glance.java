7a0da532c6e5fda068f26fa50cd381b4ddb3d549
michael.novakjr <michael.novakjr@343fa167-4643-0410-9773-4fa3183c3b48>
2008-07-19 02:52:10 +0000

    just about to actually start working on this :)

--- Glance/src/com/androidnerds/tools/Glance/Glance.java
+++ Glance/src/com/androidnerds/tools/Glance/Glance.java
@@ -1,3 +1,28 @@
+/* Copyright (c) 2008 AndroidNerds
+ *
+ * Written and Maintained by Mike Novak <mike@novaklabs.com>
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
 package com.androidnerds.tools.Glance;
 
 import android.app.Activity;
@@ -6,12 +31,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
+import android.widget.ListAdapter;
+import android.util.Log;
 
 import java.io.*;
 
 /** This is a really simple elementary application that displays directories and files on the Android system. **/
 public class Glance extends Activity
 {
+	public GridView gIconView;
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate( Bundle icicle )
@@ -20,32 +49,30 @@ public class Glance extends Activity
 		setContentView( R.layout.main );
 		
 		//grab the GridView from the layout file.
-		GridView gIconView = ( GridView )findViewById( R.id.gIconView );
+		gIconView = ( GridView )findViewById( R.id.gIconView );
 
-		//setup the default user directory if needed.
-		setUpUserDefault();
-	}
-
-	public void setUpUserDefault()
-	{
-		//check to see if it exists. if not create it.
-		File gHomeDir = new File( "/data/home/" );
-		if( gHomeDir.exists() ) {
-			return;
-		} else {
-			gHomeDir.mkdir();
-			Process gMkProc = Runtime.getRuntime().exec( "chmod 777 /data/home" );
-		}
-
-		return;
+		//get the directory listing and set it to an adapter.
+		String[] gListing = getDirectoryListing( "/" );
+		View[] gViews = prepareViewArray( gListing );
+		
 	}
 
 	public String[] getDirectoryListing( String gPath )
 	{
 		//create the file object to work with and check to see if its a directory.
 		File gCurrentFile = new File( gPath );
-		if( !gCurrentFile.isDirectory() ) return { "" };
 
 		return gCurrentFile.list();
+	}
+
+	public View[] prepareViewArray( String[] gDirListing )
+	{
+		View[] gTmpArr = new View[50];
+		int i;
+		for( i = 0; i < gDirListing.length(); i++ ) {
+			View gView = new View();
+			gView.setClickable( true );
+			
+		}
 	}
 }
