96892f5211e228a29714ba485dc79c6ea49c787e
michael.novakjr <michael.novakjr@343fa167-4643-0410-9773-4fa3183c3b48>
2008-07-26 23:06:40 +0000

    Messages: initial import of the sms project

---
+++ Messages/src/com/androidnerds/tools/Messages.java
@@ -0,0 +1,15 @@
+package com.androidnerds.tools;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Messages extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle icicle)
+    {
+        super.onCreate(icicle);
+        setContentView(R.layout.main);
+    }
+}
