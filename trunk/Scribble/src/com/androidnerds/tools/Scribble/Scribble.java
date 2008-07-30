4eaecde49684934fa1b59a642709d958ab0f53f2
michael.novakjr <michael.novakjr@343fa167-4643-0410-9773-4fa3183c3b48>
2008-07-30 20:49:07 +0000

    Scribble: added the ability to enter a file name from scratch and save a file without having to start Scribble through Glance.

--- trunk/Scribble/src/com/androidnerds/tools/Scribble/Scribble.java
+++ trunk/Scribble/src/com/androidnerds/tools/Scribble/Scribble.java
@@ -1,15 +1,182 @@
+/* Copyright (c) 2008 AndroidNerds
+ *
+ * Written and Maintained by the random guys.
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
 package com.androidnerds.tools.Scribble;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
+import android.view.Menu;
+import android.view.Menu.Item;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewInflate;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Scribble extends Activity
 {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle icicle)
-    {
-        super.onCreate(icicle);
-        setContentView(R.layout.main);
-    }
+	
+	private static final int SAVE_ID = Menu.FIRST;
+	private static final int SAVEAS_ID = Menu.FIRST + 1;
+	private static final int ABOUT_ID = Menu.FIRST + 2;
+	private File gFile;
+	private String gFilename = new String();
+	private String gFileContents;
+	private Dialog gDialog;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle icicle)
+	{
+		super.onCreate(icicle);
+        	setContentView(R.layout.main);
+
+		//we want to be able to specify a file to open off the bat if one's provided. ( i.e if Glance is trying to open a text file for us to read and/or modify. )
+		Bundle extras = getIntent().getExtras();
+	
+		if( extras != null ) {
+			gFilename = extras.getString( "filename" );
+		}
+
+		//first thing to check is see if the file is writable. If not alert the user this is read only.
+		checkFilePermissions();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu( Menu menu )
+	{
+		super.onCreateOptionsMenu( menu );
+		//TODO: make the icons have the asterisk that means 'new'
+		menu.add( 0, SAVE_ID, "Save", R.drawable.save );
+		menu.add( 0, SAVEAS_ID, "Save As...", R.drawable.saveas );
+		menu.add( 0, ABOUT_ID, "About", R.drawable.info );
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected( Menu.Item item )
+	{
+		switch( item.getId() ) {
+			case SAVE_ID:
+				writeFileToDisk();
+				break;
+			case SAVEAS_ID:
+				promptSaveDialog();
+				break;
+			case ABOUT_ID:
+				Toast.makeText( this, "Version 0.0.05\nWritten By Mike Novak\nReport bugs: mike@novaklabs.com", Toast.LENGTH_LONG ).show();
+				break;
+		}
+
+		return super.onOptionsItemSelected( item );
+	}
+
+	public void checkFilePermissions()
+	{
+		if( gFilename.equals( "" ) ) return;
+
+		gFile = new File( gFilename );
+		if( !gFile.canWrite() ) {
+			Toast.makeText( this, "Opening current file as read-only.", Toast.LENGTH_LONG ).show();
+		}
+
+		readFromFile();
+	}
+
+	public void readFromFile()
+	{
+		try {
+			byte[] buffer = new byte[ ( int )gFile.length() ];
+			FileInputStream reader = new FileInputStream( gFile );
+			reader.read( buffer );
+
+			gFileContents = new String( buffer );
+			EditText gTextField = ( EditText )findViewById( R.id.gDocumentView );
+			gTextField.setText( gFileContents );
+			setTitle( "Scribble - " + gFilename );
+			reader.close();
+		} catch( IOException e ) {
+			Log.d( "Scribble IO", "Problem parsing the file" );
+		}
+	}
+
+	public void writeFileToDisk()
+	{
+		try {
+			EditText gTextField = ( EditText )findViewById( R.id.gDocumentView );
+			String text = gTextField.getText().toString();
+
+			FileWriter writer = new FileWriter( gFile, false );
+			writer.write( text );
+			
+			writer.close();
+			Toast.makeText( this, "File has been saved.", Toast.LENGTH_LONG ).show();
+		} catch( IOException e ) {
+			Log.d( "Scribble IO", "Problem writing the file to disk" );
+		}
+	}
+
+	public void promptSaveDialog()
+	{
+		gDialog = new Dialog( this );
+		gDialog.setContentView( R.layout.savedialog );
+		gDialog.setTitle( "Save As... ( Enter full path )" );
+		
+		final Button gCreateButton = ( Button )gDialog.findViewById( R.id.gCreateButton );
+		gCreateButton.setOnClickListener( new Button.OnClickListener()  
+		{
+			public void onClick( View v )
+			{
+				saveFileAs();
+			}
+		} );
+
+		gDialog.show();
+	}
+
+	public void saveFileAs()
+	{
+		EditText filenameField = ( EditText )gDialog.findViewById( R.id.gNewFileName );
+		String filename = filenameField.getText().toString();
+		File newfile = new File( filename );
+		if( !newfile.canWrite() ) Toast.makeText( this, "No write access to " + filename + ".", Toast.LENGTH_LONG ).show();
+		if( newfile.exists() ) {
+			//oh shit!
+		}
+
+		gFile = newfile;
+		gDialog.dismiss();
+		writeFileToDisk();
+	}
 }
