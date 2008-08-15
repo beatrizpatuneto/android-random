dce6e572fd8e5c9cc150a54ee4faa577a19a986c
jasta00 <jasta00@343fa167-4643-0410-9773-4fa3183c3b48>
2008-08-15 08:10:05 +0000

    Solitaire: implemented reading and writing card stacks

--- Solitaire/src/org/devtcg/games/solitaire/game/GameOutputStream.java
+++ Solitaire/src/org/devtcg/games/solitaire/game/GameOutputStream.java
@@ -1,12 +1,55 @@
 package org.devtcg.games.solitaire.game;
 
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
+
+import org.devtcg.games.solitaire.model.Card;
+import org.devtcg.games.solitaire.model.CardStack;
 
 public class GameOutputStream extends DataOutputStream
 {
 	public GameOutputStream(OutputStream out)
 	{
 		super(out);
+	}
+	
+	public void writeCard(Card card)
+	  throws IOException
+	{
+		if (card == null)
+		{
+			writeInt(-1);
+			writeInt(-1);
+			writeBoolean(false);
+		}
+		else
+		{
+			writeInt(card.getSuit().ordinal());
+			writeInt(card.getRankOrdinal());
+			writeBoolean(card.isFaceUp());
+		}
+	}
+
+	public void writeCardStack(CardStack stack)
+	  throws IOException
+	{
+		int n = stack.size();
+
+		writeInt(n);
+
+		for (int i = 0; i < n; i++)
+			writeCard(stack.get(i));
+	}
+
+	public void writeCardStacks(CardStack[] stacks)
+	  throws IOException
+	{
+		int n = stacks.length;
+
+		writeInt(n);
+
+		for (int i = 0; i < n; i++)
+			writeCardStack(stacks[i]);
 	}
 }
