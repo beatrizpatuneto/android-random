140730ee426590b12374dec6d1098ecbd00b1c08
michael.novakjr <michael.novakjr@343fa167-4643-0410-9773-4fa3183c3b48>
2008-08-21 00:28:31 +0000

    initial import

---
+++ GoLocal/src/com/androidnerds/apps/GoLocal.java
@@ -0,0 +1,13 @@
+package com.androidnerds.apps;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GoLocal extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
