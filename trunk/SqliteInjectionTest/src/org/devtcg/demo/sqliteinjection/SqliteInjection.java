fa3b061c1aefa5a826194e5877c126912f3f59e0
jasta00 <jasta00@343fa167-4643-0410-9773-4fa3183c3b48>
2008-12-23 00:58:00 +0000

    dismiss progress dialog and fixed formatting

--- trunk/SqliteInjectionTest/src/org/devtcg/demo/sqliteinjection/SqliteInjection.java
+++ trunk/SqliteInjectionTest/src/org/devtcg/demo/sqliteinjection/SqliteInjection.java
@@ -46,123 +46,125 @@ public class SqliteInjection extends Activity
 	{
 		Log.i(TAG, "Handing out a new cursor...");
 
-        Cursor c = getContentResolver().query(Schema.Foo.CONTENT_URI,
-          QUERY_FIELDS, null, null, null);
+		Cursor c = getContentResolver().query(Schema.Foo.CONTENT_URI,
+		  QUERY_FIELDS, null, null, null);
 
-        c.registerContentObserver(new ContentObserver(new Handler()) {
-        	public void onChange(boolean selfChange) {
-        		Log.d(TAG, "onChange: selfChange=" + selfChange);
-        	}
-        });
-        
-        return c;
+		c.registerContentObserver(new ContentObserver(new Handler()) {
+			public void onChange(boolean selfChange) {
+				Log.d(TAG, "onChange: selfChange=" + selfChange);
+			}
+		});
+		
+		return c;
 	}
 
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        mGetDb = (Button)findViewById(R.id.getDatabase);
-        mGetDb.setOnClickListener(mClick);
-        
-        mReQueryDb = (Button)findViewById(R.id.queryDatabase);
-        mReQueryDb.setOnClickListener(mClick);
-        
-        mList = (ListView)findViewById(R.id.list);
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		mGetDb = (Button)findViewById(R.id.getDatabase);
+		mGetDb.setOnClickListener(mClick);
+		
+		mReQueryDb = (Button)findViewById(R.id.queryDatabase);
+		mReQueryDb.setOnClickListener(mClick);
+		
+		mList = (ListView)findViewById(R.id.list);
 
-        Cursor c = getCursor();
+		Cursor c = getCursor();
 
-        mAdapter = new SimpleCursorAdapter(this, R.layout.foo_item, c,
-          new String[] { Schema.Foo.NAME, Schema.Foo.FOO, Schema.Foo.BAR,
-            Schema.Foo.BAZ },
-          new int[] { R.id.foo_name, R.id.foo_foo, R.id.foo_bar,
-            R.id.foo_baz });
+		mAdapter = new SimpleCursorAdapter(this, R.layout.foo_item, c,
+		  new String[] { Schema.Foo.NAME, Schema.Foo.FOO, Schema.Foo.BAR,
+			Schema.Foo.BAZ },
+		  new int[] { R.id.foo_name, R.id.foo_foo, R.id.foo_bar,
+			R.id.foo_baz });
 
-        mAdapter.registerDataSetObserver(new DataSetObserver() {
-        	public void onChanged() {
-        		Log.e(TAG, "onChanged");
-        	}
+		mAdapter.registerDataSetObserver(new DataSetObserver() {
+			public void onChanged() {
+				Log.e(TAG, "onChanged");
+			}
 
-        	public void onInvalidated() {
-        		Log.e(TAG, "onInvalidated");
-        	}
-        });
-        
-        mList.setAdapter(mAdapter);
-    }
-    
-    @Override
-    protected void onDestroy()
-    {
-    	mAdapter.changeCursor(null);
-    	super.onDestroy();
-    }
+			public void onInvalidated() {
+				Log.e(TAG, "onInvalidated");
+			}
+		});
+		
+		mList.setAdapter(mAdapter);
+	}
+	
+	@Override
+	protected void onDestroy()
+	{
+		mAdapter.changeCursor(null);
+		super.onDestroy();
+	}
 
-    private final OnClickListener mClick = new OnClickListener()
-    {
-    	public void onClick(View v)
-    	{
-    		switch (v.getId())
-    		{
-    		case R.id.getDatabase:
-    			mProgress = ProgressDialog.show(SqliteInjection.this,
-    			  "Downloading", "Downloading database...");
-    			(new ReplaceDbThread()).start();
-    			break;
-    		case R.id.queryDatabase:
-    			mAdapter.getCursor().requery();
-    			break;
-    		}
-    	}
-    };
+	private final OnClickListener mClick = new OnClickListener()
+	{
+		public void onClick(View v)
+		{
+			switch (v.getId())
+			{
+			case R.id.getDatabase:
+				mProgress = ProgressDialog.show(SqliteInjection.this,
+				  "Downloading", "Downloading database...");
+				(new ReplaceDbThread()).start();
+				break;
+			case R.id.queryDatabase:
+				mAdapter.getCursor().requery();
+				break;
+			}
+		}
+	};
 
-    private class ReplaceDbThread extends Thread
-    {
-    	public void run()
-    	{
-    		URL url;
-    		
-    		InputStream in = null;
-    		OutputStream out = null;
+	private class ReplaceDbThread extends Thread
+	{
+		public void run()
+		{
+			URL url;
+			
+			InputStream in = null;
+			OutputStream out = null;
 
-    		try {
+			try {
 				url = new URL("http://jasta.dyndns.org/android/foo.db");
-   				
+				
 				File dbPath = getDatabasePath("foo-remote.db");
-   				
+				
 				in = url.openStream();
 				out = new FileOutputStream(dbPath);
-   				
+				
 				byte[] b = new byte[2048];
 				int n;
 
 				while ((n = in.read(b)) >= 0)
 					out.write(b, 0, n);
 
-   				Log.i(TAG, "Installing database...");
-   				ContentValues cv = new ContentValues();
-   				cv.put(Provider.EXTERNAL_DATABASE_PATH,
-   				  dbPath.getAbsolutePath());
-   				getContentResolver().insert(Schema.Foo.CONTENT_URI, cv);
+				Log.i(TAG, "Installing database...");
+				ContentValues cv = new ContentValues();
+				cv.put(Provider.EXTERNAL_DATABASE_PATH,
+				  dbPath.getAbsolutePath());
+				getContentResolver().insert(Schema.Foo.CONTENT_URI, cv);
 
-   				mProgress.dismiss();
-    		} catch (Exception e) {
-    			final String msg = e.toString();
-    			mHandler.post(new Runnable() {
-    				public void run() {
-    	    			Toast.makeText(SqliteInjection.this, "Fuck: " + msg,
-    	    	    	  Toast.LENGTH_LONG).show();
-    				}
-    			});
-    		} finally {
-    			if (in != null)
-    				try { in.close(); } catch (IOException e) {}
-    			
-    			if (out != null)
-    				try { out.close(); } catch (IOException e) {}
-    		}
-    	}
-    }
+				mProgress.dismiss();
+			} catch (Exception e) {
+				final String msg = e.toString();
+				mHandler.post(new Runnable() {
+					public void run() {
+						Toast.makeText(SqliteInjection.this, "Fuck: " + msg,
+						  Toast.LENGTH_LONG).show();
+					}
+				});
+			} finally {
+				if (in != null)
+					try { in.close(); } catch (IOException e) {}
+
+				if (out != null)
+					try { out.close(); } catch (IOException e) {}
+
+				mProgress.dismiss();
+			}
+		}
+	}
 }
