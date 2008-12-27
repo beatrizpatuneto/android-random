1565d744a19031d7ec3c10b16d04d818befe9185
michael.novakjr <michael.novakjr@343fa167-4643-0410-9773-4fa3183c3b48>
2008-12-27 08:36:10 +0000

    RemoteLogger runs on devices now, builds against 1.0 SDK

--- RemoteLogger/client/src/com/androidnerds/tools/RemoteLogger/RemoteLogger.java
+++ RemoteLogger/client/src/com/androidnerds/tools/RemoteLogger/RemoteLogger.java
@@ -1,27 +1,25 @@
+/**
+ * Written by Mike Novak - michael.novakjr@gmail.com
+ *
+ * Submit issues to http://code.google.com/p/android-random
+ */
 package com.androidnerds.tools.RemoteLogger;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.ComponentName;
-import android.content.ServiceConnection;
-import android.content.Context;
 import android.os.Bundle;
-import android.os.DeadObjectException;
-import android.os.IBinder;
 import android.text.Editable;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class RemoteLogger extends Activity
 {
-	private LogProcessorInterface logInterface;
 	private String serverAddress;
 	private String serverPort;
 
-	private boolean isBinded = false;
-
+	private Intent mService;
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate( Bundle icicle )
@@ -51,16 +49,13 @@ public class RemoteLogger extends Activity
 	public void onPause()
 	{
 		super.onPause();
-		if( isBinded ) unbindService( gConnection );
+		//if( isBinded ) unbindService( gConnection );
 	}
 
-	@Override
-	public void onResume()
-	{
-		super.onResume();
-		workThatMagic();
+	public void stopLogService() {
+		stopService(mService);
 	}
-
+	
 	public void workThatMagic()
 	{
 		EditText gAddressField = ( EditText )findViewById( R.id.serverAddr );
@@ -71,32 +66,14 @@ public class RemoteLogger extends Activity
 		Editable gPort = gPortField.getText();
 		serverPort = gPort.toString();
 
+		mService = new Intent(this, LogProcessor.class);
+		mService.putExtra("address", serverAddress);
+		mService.putExtra("port", serverPort);
 		
-		if( !serverAddress.equals("") ) this.bindService( new Intent( LogProcessorInterface.class.getName() ), gConnection, Context.BIND_AUTO_CREATE );
+		startService(mService);
+		
+		
+		//if( !serverAddress.equals("") ) this.bindService( new Intent( LogProcessorInterface.class.getName() ), gConnection, Context.BIND_AUTO_CREATE );
 		//oh yea that's all for this method :)
 	}
-
-	private void stopLogService()
-	{
-		if( isBinded ) unbindService( gConnection );
-	}
-
-	private ServiceConnection gConnection = new ServiceConnection() 
-	{
-		public void onServiceConnected( ComponentName className, IBinder service ) 
-		{
-			logInterface = LogProcessorInterface.Stub.asInterface( service );
-			try {
-				isBinded = true;
-				logInterface.startTheCat( serverAddress, serverPort );
-			} catch( DeadObjectException e ) {
-
-			}
-		}
-
-		public void onServiceDisconnected( ComponentName className )
-		{
-			isBinded = false;
-		}
-	};
 }
